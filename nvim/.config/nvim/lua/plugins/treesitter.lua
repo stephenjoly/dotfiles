5c86718962131aacfb3800db73de5a77cb5d2690
Stephen <stephen.a.joly@gmail.com>
2024-07-06 17:58:47 -0400

    Adding the new nvim configs
    + additional changes to zsh files

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      auto_install = true,
+      -- ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
+}
---
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,10 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+--vim.wo.relativenumber = true
+vim.wo.nu = true
+vim.wo.rnu = true
+
